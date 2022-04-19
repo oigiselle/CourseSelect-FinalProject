5d7ed0614062221dbe5f8124d6cd6bea4c3805fc
gisellesalvador <gigs1990@outlook.com>
2022-04-19 11:39:40 -0400

    Main layout and first layout created

--- app/src/main/java/com/ggslvdr/courseselect_finalproject/FirstMenu.java
+++ app/src/main/java/com/ggslvdr/courseselect_finalproject/FirstMenu.java
@@ -3,15 +3,12 @@ package com.ggslvdr.courseselect_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ImageButton loginbtn, contactusbtn;
+public class FirstMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_menu);
     }
 }
